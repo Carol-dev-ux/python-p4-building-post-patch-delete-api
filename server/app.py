bd0a38abaff874f95e4ce4816497226dca82bf12
Caroline Wangari <karehwachira@gmail.com>
2024-04-15 15:59:58 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db, User, Review, Game
+from models import db, User, Review, Game, Bakery, BakedGood  # Import Bakery and BakedGood models
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,7 +19,6 @@ def index():
 
 @app.route('/games')
 def games():
-
     games = []
     for game in Game.query.all():
         game_dict = game.to_dict()
@@ -47,7 +46,6 @@ def game_by_id(id):
 
 @app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
-
     if request.method == 'GET':
         reviews = []
         for review in Review.query.all():
@@ -60,7 +58,6 @@ def reviews():
         )
 
         return response
-
     elif request.method == 'POST':
         new_review = Review(
             score=request.form.get("score"),
@@ -92,7 +89,6 @@ def review_by_id(id):
         response = make_response(response_body, 404)
 
         return response
-
     else:
         if request.method == 'GET':
             review_dict = review.to_dict()
@@ -103,7 +99,6 @@ def review_by_id(id):
             )
 
             return response
-
         elif request.method == 'PATCH':
             for attr in request.form:
                 setattr(review, attr, request.form.get(attr))
@@ -119,7 +114,6 @@ def review_by_id(id):
             )
 
             return response
-
         elif request.method == 'DELETE':
             db.session.delete(review)
             db.session.commit()
@@ -138,7 +132,6 @@ def review_by_id(id):
 
 @app.route('/users')
 def users():
-
     users = []
     for user in User.query.all():
         user_dict = user.to_dict()
@@ -151,6 +144,78 @@ def users():
 
     return response
 
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    new_baked_good = BakedGood(
+        name=request.form.get("name"),
+        description=request.form.get("description"),
+        bakery_id=request.form.get("bakery_id"),
+        price=request.form.get("price")
+    )
+
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    baked_good_dict = new_baked_good.to_dict()
+
+    response = make_response(
+        baked_good_dict,
+        201
+    )
+
+    return response
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    if bakery is None:
+        response_body = {
+            "message": "This bakery does not exist in our database. Please try again."
+        }
+        response = make_response(response_body, 404)
+        return response
+    else:
+        for attr in request.form:
+            setattr(bakery, attr, request.form.get(attr))
+
+        db.session.add(bakery)
+        db.session.commit()
+
+        bakery_dict = bakery.to_dict()
+
+        response = make_response(
+            bakery_dict,
+            200
+        )
+
+        return response
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.filter(BakedGood.id == id).first()
+
+    if baked_good is None:
+        response_body = {
+            "message": "This baked good does not exist in our database. Please try again."
+        }
+        response = make_response(response_body, 404)
+        return response
+    else:
+        db.session.delete(baked_good)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Baked good deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
